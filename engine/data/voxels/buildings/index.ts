b13df4e27187260e84cb57d690c16f3fe7f36f05
Klaasvaakie <49187633+WimpyvL@users.noreply.github.com>
2026-06-07 16:35:42 +0530

    Register infrastructure assets as a separate building family

--- engine/data/voxels/buildings/index.ts
+++ engine/data/voxels/buildings/index.ts
@@ -1,5 +1,5 @@
-
 import { CommonBuildings } from './common';
+import { InfrastructureBuildings } from './infrastructure';
 import { Era1Buildings } from './era1';
 import { Era2Buildings } from './era2';
 import { Era3Buildings } from './era3';
@@ -8,10 +8,10 @@ import { Era5Buildings } from './era5';
 
 export const BuildingsFactory = {
     ...CommonBuildings,
+    ...InfrastructureBuildings,
     ...Era1Buildings,
     ...Era2Buildings,
     ...Era3Buildings,
     ...Era4Buildings,
     ...Era5Buildings
 };
-
