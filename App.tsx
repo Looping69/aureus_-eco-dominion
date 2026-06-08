8fd64a5ccd322e12240eeb9a662863ece3c4969e
Klaasvaakie <49187633+WimpyvL@users.noreply.github.com>
2026-06-08 17:17:06 +0200

    Show AI overseer controls in game HUD

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import { InventoryHUD } from './components/InventoryHUD';
 import { NewsTicker } from './components/NewsTicker';
 import { GoalWidget } from './components/GoalWidget';
 import { ContractTracker } from './components/ContractTracker';
+import { AIOverseerPanel } from './components/AIOverseerPanel';
 import {
     TutorialOverlay,
     ConstructionModal,
@@ -592,6 +593,7 @@ const App: React.FC = () => {
                                                     <div className="absolute top-14 left-2 sm:left-4 z-40 flex flex-col gap-2 items-start pointer-events-none">
                                                         <TutorialOverlay step={state.step} dispatch={dispatch} setSidebarOpen={handleSidebarOpen} playSfx={playSfx} />
                                                         <GoalWidget goal={state.activeGoal} dispatch={dispatch} playSfx={playSfx} />
+                                                        <AIOverseerPanel state={state} world={worldInstance} playSfx={playSfx} />
                                                         <ContractTracker state={state} world={worldInstance} playSfx={playSfx} />
                                                         <NewsTicker news={state.newsFeed} onDismiss={(id) => dispatch({ type: 'DISMISS_NEWS', payload: id })} playSfx={playSfx} />
                                                     </div>
