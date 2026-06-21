3bd5ea7c89c9e127935e2189abc91a46316d80af
Klaasvaakie <49187633+WimpyvL@users.noreply.github.com>
2026-06-21 10:24:53 +0200

    Use type-only fog model imports

--- engine/sim/fogOfWar/FogOfWarModel.ts
+++ engine/sim/fogOfWar/FogOfWarModel.ts
@@ -1,4 +1,5 @@
-import { BuildingType, GameState } from '../../../types';
+import { BuildingType } from '../../../types';
+import type { GameState } from '../../../types';
 import { CHUNK_SIZE, ChunkStore } from '../../space/ChunkStore';
 import { toChunkKey, worldToChunk } from '../../utils/coords';
 
