a3a0da8d0019db90488ec63cb3a1c4dbaca89b6f
Klaasvaakie <49187633+WimpyvL@users.noreply.github.com>
2026-06-30 16:01:34 +0200

    Move utility amounts into resource grid roles

--- engine/data/resourceGridRoles.ts
+++ engine/data/resourceGridRoles.ts
@@ -14,6 +14,8 @@ export interface ResourceGridBuildingRoleDef {
     serviceRadius?: number;
     serviceMetric?: ResourceGridServiceMetric;
     priority?: number;
+    baseProduction?: number;
+    baseDemand?: number;
     productionModifiers?: ResourceGridProductionModifier[];
 }
 
@@ -30,60 +32,102 @@ export const RESOURCE_GRID_BUILDING_ROLES: Partial<Record<BuildingType, Resource
         { networkType: 'power', roles: ['CARRIER'], serviceRadius: 1, serviceMetric: 'MANHATTAN' },
     ],
     [BuildingType.POND]: [
-        { networkType: 'water', roles: ['PRODUCER', 'CARRIER'], serviceRadius: 3, serviceMetric: 'CHEBYSHEV', productionModifiers: ['POND_WEATHER'] },
+        { networkType: 'water', roles: ['PRODUCER', 'CARRIER'], baseProduction: 5, serviceRadius: 3, serviceMetric: 'CHEBYSHEV', productionModifiers: ['POND_WEATHER'] },
     ],
     [BuildingType.WATER_WELL]: [
-        { networkType: 'water', roles: ['PRODUCER', 'CARRIER'], serviceRadius: 3, serviceMetric: 'CHEBYSHEV' },
+        { networkType: 'water', roles: ['PRODUCER', 'CARRIER'], baseProduction: 10, serviceRadius: 3, serviceMetric: 'CHEBYSHEV' },
     ],
     [BuildingType.RESERVOIR]: [
-        { networkType: 'water', roles: ['PRODUCER', 'CARRIER'], serviceRadius: 3, serviceMetric: 'CHEBYSHEV', productionModifiers: ['RESERVOIR_POWER_DEPENDENCY'] },
-        { networkType: 'power', roles: ['CONSUMER'], priority: 100 },
+        { networkType: 'water', roles: ['PRODUCER', 'CARRIER'], baseProduction: 50, serviceRadius: 3, serviceMetric: 'CHEBYSHEV', productionModifiers: ['RESERVOIR_POWER_DEPENDENCY'] },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 2, priority: 100 },
     ],
     [BuildingType.SOLAR_ARRAY]: [
-        { networkType: 'power', roles: ['PRODUCER', 'CARRIER'], serviceRadius: 1, serviceMetric: 'MANHATTAN', productionModifiers: ['SOLAR_DAYLIGHT'] },
+        { networkType: 'power', roles: ['PRODUCER', 'CARRIER'], baseProduction: 5, serviceRadius: 1, serviceMetric: 'MANHATTAN', productionModifiers: ['SOLAR_DAYLIGHT'] },
     ],
     [BuildingType.WIND_TURBINE]: [
-        { networkType: 'power', roles: ['PRODUCER', 'CARRIER'], serviceRadius: 1, serviceMetric: 'MANHATTAN', productionModifiers: ['WIND_WEATHER'] },
+        { networkType: 'power', roles: ['PRODUCER', 'CARRIER'], baseProduction: 8, serviceRadius: 1, serviceMetric: 'MANHATTAN', productionModifiers: ['WIND_WEATHER'] },
     ],
     [BuildingType.GENERATOR]: [
-        { networkType: 'power', roles: ['PRODUCER', 'CARRIER'], serviceRadius: 1, serviceMetric: 'MANHATTAN' },
+        { networkType: 'power', roles: ['PRODUCER', 'CARRIER'], baseProduction: 10, serviceRadius: 1, serviceMetric: 'MANHATTAN' },
     ],
     [BuildingType.GEOTHERMAL_PLANT]: [
-        { networkType: 'power', roles: ['PRODUCER', 'CARRIER'], serviceRadius: 1, serviceMetric: 'MANHATTAN' },
+        { networkType: 'power', roles: ['PRODUCER', 'CARRIER'], baseProduction: 50, serviceRadius: 1, serviceMetric: 'MANHATTAN' },
     ],
     [BuildingType.STAFF_QUARTERS]: [
-        { networkType: 'water', roles: ['CONSUMER'], priority: 100 },
-        { networkType: 'power', roles: ['CONSUMER'], priority: 90 },
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 1, priority: 100 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 1, priority: 90 },
+    ],
+    [BuildingType.CANTEEN]: [
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 2 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 2 },
     ],
     [BuildingType.COMMUNITY_GARDEN]: [
         { networkType: 'water', roles: ['CONSUMER'], priority: 95 },
     ],
     [BuildingType.WASTE_TREATMENT]: [
-        { networkType: 'water', roles: ['CONSUMER'], priority: 90 },
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 5, priority: 90 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 8 },
     ],
     [BuildingType.GREEN_TECH_LAB]: [
         { networkType: 'water', roles: ['CONSUMER'], priority: 80 },
         { networkType: 'power', roles: ['CONSUMER'], priority: 70 },
     ],
     [BuildingType.WASH_PLANT]: [
-        { networkType: 'water', roles: ['CONSUMER'], priority: 65 },
-        { networkType: 'power', roles: ['CONSUMER'], priority: 70 },
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 5, priority: 65 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 5, priority: 70 },
     ],
     [BuildingType.RECYCLING_PLANT]: [
-        { networkType: 'water', roles: ['CONSUMER'], priority: 65 },
-        { networkType: 'power', roles: ['CONSUMER'], priority: 70 },
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 3, priority: 65 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 8, priority: 70 },
     ],
     [BuildingType.ORE_FOUNDRY]: [
         { networkType: 'water', roles: ['CONSUMER'], priority: 65 },
-        { networkType: 'power', roles: ['CONSUMER'], priority: 70 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 15, priority: 70 },
     ],
     [BuildingType.GEM_REFINERY]: [
-        { networkType: 'water', roles: ['CONSUMER'], priority: 65 },
-        { networkType: 'power', roles: ['CONSUMER'], priority: 70 },
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 3, priority: 65 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 10, priority: 70 },
     ],
     [BuildingType.WORKSHOP]: [
         { networkType: 'water', roles: ['CONSUMER'], priority: 65 },
         { networkType: 'power', roles: ['CONSUMER'], priority: 70 },
+    ],
+    [BuildingType.MINING_HEADFRAME]: [
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 2 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 10 },
+    ],
+    [BuildingType.SAWMILL]: [
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 5 },
+    ],
+    [BuildingType.STONE_QUARRY]: [
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 6 },
+    ],
+    [BuildingType.SURVEY_DRILL]: [
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 2 },
+    ],
+    [BuildingType.MEDICAL_BAY]: [
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 3 },
+    ],
+    [BuildingType.TRAINING_CENTER]: [
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 2 },
+    ],
+    [BuildingType.TRAIN_STATION]: [
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 2 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 10 },
+    ],
+    [BuildingType.DRONE_DEPOT]: [
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 1 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 14 },
+    ],
+    [BuildingType.DISTRIBUTION_HUB]: [
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 5 },
+    ],
+    [BuildingType.HYDROPONICS]: [
+        { networkType: 'water', roles: ['CONSUMER'], baseDemand: 8 },
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 4 },
+    ],
+    [BuildingType.SPACEPORT]: [
+        { networkType: 'power', roles: ['CONSUMER'], baseDemand: 100 },
     ],
 };
 
