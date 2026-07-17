8d4b48a733335e5a920bb65b6e96497193c158e6
Klaasvaakie <49187633+WimpyvL@users.noreply.github.com>
2026-07-18 00:49:54 +0200

    Activate build action payload schemas in the Aureus game pack

--- game-definitions/activeGameDefinition.ts
+++ game-definitions/activeGameDefinition.ts
@@ -1,10 +1,13 @@
 import { createGameDefinitionRegistry } from '../engine/game-definition/GameDefinitionRegistry';
 import { AUREUS_GAME_DEFINITION } from './aureus';
+import { withAureusBuildActionPayloadSchemas } from './aureusBuildActionPayloadSchemas';
 
-export const GAME_DEFINITION_REGISTRY = createGameDefinitionRegistry([AUREUS_GAME_DEFINITION]);
+const AUREUS_ACTIVE_GAME_DEFINITION = withAureusBuildActionPayloadSchemas(AUREUS_GAME_DEFINITION);
+
+export const GAME_DEFINITION_REGISTRY = createGameDefinitionRegistry([AUREUS_ACTIVE_GAME_DEFINITION]);
 
 export function getActiveGameDefinition() {
-  return GAME_DEFINITION_REGISTRY.getActive() ?? AUREUS_GAME_DEFINITION;
+  return GAME_DEFINITION_REGISTRY.getActive() ?? AUREUS_ACTIVE_GAME_DEFINITION;
 }
 
 export function getActiveGameDefinitionSummary() {
