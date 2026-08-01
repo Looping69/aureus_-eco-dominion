efb2894d6be1417b8bcc47a4d54e7ba9e8d8723e
Klaasvaakie <49187633+WimpyvL@users.noreply.github.com>
2026-08-01 23:49:47 +0200

    Export generic game pack boundary

---
+++ engine/game-pack/index.ts
@@ -0,0 +1,12 @@
+export {
+  GamePackRegistry,
+  createGamePackRegistry,
+  defineGamePack,
+  summarizeGamePack,
+} from './GamePack';
+export type {
+  GamePack,
+  GamePackId,
+  GamePackRuntimeDefinition,
+  GamePackSummary,
+} from './GamePack';
